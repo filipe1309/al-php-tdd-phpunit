2399886fd1f1cbafb85d64b0b320d2b0ef245ec0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-17 17:44:25 -0300

    Add comments

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -5,6 +5,9 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+# Teste unitario no padrao Arrange Act Assert, ou GivenWhenThen (Martin Fowler)
+
+# Arrange/Given - A inicialização do cenário
 $leilao = new Leilao('Fiat 147 0km');
 
 $maria = new Usuario('Maria');
@@ -14,11 +17,16 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+# Act/When - A execução da regra de negócio
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-if ($maiorValor === 2500.0) {
+# Assert/Then - A verificação do resultado
+$valorEsperado = (float) 2500;
+
+if ($maiorValor === $valorEsperado) {
     echo "TESTE OK";
 } else {
     echo "TESTE FALHOU";
